cad201b64e304b8c65d5d0dd4d0339167e74dad4
Anja van Gestel <a.vangestel@iknl.nl>
2023-10-23 10:28:45 +0200

    Added simplify in variable_categories because the data type of categories was dependent on the explanatory input variables

--- models/crosstab/src/R/variable_categories.R
+++ models/crosstab/src/R/variable_categories.R
@@ -13,7 +13,7 @@
 #'
 variable_categories <- function(nodes, master){
     categories <- sapply(all.vars(master$formula), function(i){
-        unique(as.vector(sapply(nodes, function(j) j[[i]])))
+        unique(as.vector(sapply(nodes, function(j) j[[i]], simplify=F)))
     })
     master$var_cat <- categories
     return(master)
